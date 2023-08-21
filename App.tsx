6140a208eeb038828b2de7665a885d9b7f3d83b1
Krinesh <pkrinesh95@gmail.com>
2023-08-21 22:06:28 +0530

    feat: `expo-router`

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Toolbar from './src/screens/toolbar';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Toolbar from './src/app/toolbar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-      <Toolbar />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar style="auto" />
+//       {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+//       <Toolbar />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
