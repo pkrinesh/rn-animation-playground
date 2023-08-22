3c5b2b65df65eda7ddc7b88c006ae245597f8316
Krinesh <pkrinesh95@gmail.com>
2023-08-22 15:27:58 +0530

    feat: routes list for easy adding new route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,36 @@
-import { Link, Stack } from 'expo-router';
+import { Link, LinkComponent, LinkProps, Stack } from 'expo-router';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-export default function Index() {
+type RouteConfig = {
+  name: string;
+  href: LinkProps<string>['href'];
+};
+
+const routes: RouteConfig[] = [
+  {
+    name: 'Toolbar',
+    href: '/toolbar',
+  },
+];
+
+export default function Home() {
   const [isPressed, setIsPressed] = useState(false);
   return (
     <View style={[styles.container]}>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Link
-        href="/toolbar"
-        style={[styles.linkButton, { opacity: isPressed ? 0.6 : 1 }]}
-        onPressIn={() => {
-          setIsPressed(true);
-        }}
-      >
-        Toolbar
-      </Link>
+      {routes.map(({ name, href }) => (
+        <Link
+          key={href}
+          href={href}
+          style={[styles.linkButton, { opacity: isPressed ? 0.6 : 1 }]}
+          onPressIn={() => {
+            setIsPressed(true);
+          }}
+        >
+          {name}
+        </Link>
+      ))}
     </View>
   );
 }
